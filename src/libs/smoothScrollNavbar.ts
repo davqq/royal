80706997bea9c207d17111ad162bc11e075eadb6
davqq <davidvongarrel10219@gmail.com>
2023-10-01 12:55:08 +0200

    change navbar

---
+++ src/libs/smoothScrollNavbar.ts
@@ -0,0 +1,29 @@
+// Smooth Scrolling für die Navbar-Links
+
+function scroll() {
+  return document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (
+        anchor instanceof HTMLAnchorElement &&
+        anchor !== null &&
+        anchor.hasAttribute("href")
+      ) {
+        const targetId = anchor.getAttribute("href")?.substring(1);
+        const targetElement = document.getElementById(targetId || "");
+        const header = document.getElementById("header");
+
+        if (targetElement !== null && header instanceof HTMLElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - header.offsetHeight,
+            behavior: "smooth",
+          });
+        }
+      }
+    });
+  });
+}
+
+scroll();
+document.addEventListener("astro:after-swap", () => scroll());
